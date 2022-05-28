13d06c3881a89725b0ff94aa1c84d4bca7355a73
mu <1918129147@qq.com>
2022-05-28 20:40:02 +0800

    mjdud

--- src/main/java/net/hongye/rickroll/ExampleMod.java
+++ src/main/java/net/hongye/rickroll/ExampleMod.java
@@ -1,11 +1,19 @@
 package net.hongye.rickroll;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.event.player.UseEntityCallback;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.hongye.rickroll.register.EventRgister;
 import net.hongye.rickroll.register.ItemRegister;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,5 +32,19 @@ public class ExampleMod implements ModInitializer {
 
 		LOGGER.info("Hello Fabric world!");
 
+		UseBlockCallback.EVENT.register((player, world, hand, hitResult) ->
+		{
+			BlockHitResult blockHitResult = (BlockHitResult) hitResult;
+            /* Manual spectator check is necessary because AttackBlockCallbacks
+               fire before the spectator check */
+			//if (!player.isSpectator() &&
+					//player.getMainHandStack().isEmpty())
+
+			player.damage(DamageSource.GENERIC, 1.0F);
+
+			return ActionResult.FAIL;
+		});
 	}
+
+		;
 }
