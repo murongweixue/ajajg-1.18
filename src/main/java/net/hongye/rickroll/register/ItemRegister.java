084196d5bbe09cedbf05c64efa85c135affe1b18
mu <1918129147@qq.com>
2022-06-25 15:10:26 +0800

    mjdud

--- src/main/java/net/hongye/rickroll/register/ItemRegister.java
+++ src/main/java/net/hongye/rickroll/register/ItemRegister.java
@@ -2,6 +2,7 @@ package net.hongye.rickroll.register;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.hongye.rickroll.CustomItem;
+import net.hongye.rickroll.items.Bow;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -10,10 +11,13 @@ public class ItemRegister {
 
     public static final CustomItem CUSTOM_ITEM = new CustomItem(new FabricItemSettings().group(ItemGroup.MISC));
 
+    public static final Bow BOW =new Bow(new FabricItemSettings().group(ItemGroup.MISC));
+
         public static void registerItems() {
 
             Registry.register(Registry.ITEM, new Identifier("rickroll", "custom_item"), CUSTOM_ITEM);
 
+            Registry.register(Registry.ITEM, new Identifier("rickroll","boooow"),BOW);
 
         }
 }
